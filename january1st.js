8efa2050f3ce0d109829409edc03d678afb9338e
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-26 22:33:06 +0900

    Create january1st.js

---
+++ january1st.js
@@ -0,0 +1,15 @@
+// Show game popup
+const eventDate = 1735689600000
+const popup = document.getElementById("popup")
+popup.showModal()
+
+setInterval(() => {
+    august21PopupTimer.textContent = ` (${toCountdownString(eventDate)})`
+}, 1000)
+
+startCountDown(eventDate, false).then((async) => {
+    august21PopupTimer.style.display = "none"
+    // TODO: Reimplement on game release
+    august21PopupLabel.style.display = "none"
+    august21PopupButton.style.display = "flex"
+})
